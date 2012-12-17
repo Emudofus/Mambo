51b1b9130a76742dc49b2d387e631081174f7007
Blackrush <blackrushx@gmail.com>
2012-12-17 21:49:47 +0100

    génères la clé publique RSA

--- mambo-core/mambo-core-login/src/main/java/org/mambo/core/login/service/login/crypto/DefaultLoginCryptoService.java
+++ mambo-core/mambo-core-login/src/main/java/org/mambo/core/login/service/login/crypto/DefaultLoginCryptoService.java
@@ -1,8 +1,14 @@
 package org.mambo.core.login.service.login.crypto;
 
 import org.jetbrains.annotations.NotNull;
+import org.mambo.core.configuration.ConfigurationProvider;
 import org.mambo.core.login.database.model.User;
+import org.mambo.shared.database.Repository;
 
+import javax.inject.Inject;
+import java.security.KeyPair;
+import java.security.KeyPairGenerator;
+import java.security.NoSuchAlgorithmException;
 import java.security.PublicKey;
 
 /**
@@ -12,17 +18,38 @@ import java.security.PublicKey;
  * Time: 21:51
  */
 public class DefaultLoginCryptoService implements LoginCryptoService {
+    public static final int DEFAULT_KEYSIZE = 2048;
 
+    private static KeyPair generateKeyPair(int keysize) {
+        KeyPairGenerator generator;
+        try {
+            generator = KeyPairGenerator.getInstance("RSA");
+        } catch (NoSuchAlgorithmException e) {
+            throw new RuntimeException("unknown RSA algorithm", e);
+        }
+
+        generator.initialize(keysize);
+        return generator.generateKeyPair();
+    }
+
+    private final KeyPair keys;
+    private final Repository<User> users;
+
+    @Inject
+    public DefaultLoginCryptoService(ConfigurationProvider config, Repository<User> users) {
+        this.keys = generateKeyPair(config.getInt("services.login.crypto.keysize", DEFAULT_KEYSIZE));
+        this.users = users;
+    }
 
     @NotNull
     @Override
     public PublicKey getPublicKey() {
-        return null;  //To change body of implemented methods use File | Settings | File Templates.
+        return keys.getPublic();
     }
 
     @NotNull
     @Override
     public User find(byte[] bytes) {
-        return null;  //To change body of implemented methods use File | Settings | File Templates.
+        return users.find("username", "foo"); // TODO
     }
 }
